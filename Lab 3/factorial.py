5e85c2c327b521c35129ed3ec5f13f3ebc066089
Mahesh <115026231+DevMac2@users.noreply.github.com>
2023-02-04 20:32:09 +0545

    Update factorial.py

--- Lab 3/factorial.py
+++ Lab 3/factorial.py
@@ -1,7 +1,12 @@
+# Python program to find factorial of given number
+
+# Function to find factorial of given number
 def factorial(n):
  if(n==0):
-    return 1
+    return 1    # Base case to stop the iteration
  else:
-    return n*factorial(n-1)
+    return n*factorial(n-1)   # Recursive Call
+
+# Driver Code
 num=int(input("Enter a number: "))
 print("The factorial of", num ,"is",factorial(num))
