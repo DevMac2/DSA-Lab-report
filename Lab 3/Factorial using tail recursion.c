f11256d7963287e7983c267fb74f06c554f4cf09
Mahesh <115026231+DevMac2@users.noreply.github.com>
2023-02-04 20:23:33 +0545

    Update Factorial using tail recursion.c

--- Lab 3/Factorial using tail recursion.c
+++ Lab 3/Factorial using tail recursion.c
@@ -1,11 +1,15 @@
+//Factorial of any number 
+//Implementation of tail recursion
 #include<stdio.h>
-long int tailfact(int n,long int a)
+long int tailfact(int n,long int a)   //Function to find factorial of given number
 {
     if(n==0)
-    return a;
+    return a;     //base condition to stop the iteration
     else
-    return tailfact(n-1,n*a);
+    return tailfact(n-1,n*a);    //recursive call
 }
+
+//Driver Code
 int main()
 {
     int n;
